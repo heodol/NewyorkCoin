da9c0c7145201188224dbcb900bee75dab9f89d2
MrSlosh <jason@blackboxrealities.com>
2018-07-24 13:38:23 -0500

    attempting to add qt46 for mac

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -5,7 +5,7 @@ qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf
 
 qt_linux_packages= qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
-qt_darwin_packages=qt
+qt_darwin_packages=qt qt46
 qt_mingw32_packages=qt
 
 
