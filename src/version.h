73333430fb3f733da71c423e6037ca71b56e6544
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2021-04-09 10:57:24 +0100

    Minor updates

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
 
 // XXX: Decide if this is appropriate - if we reintroduce alerts we may need
 //      to  reduce to 70012
-static const int PROTOCOL_VERSION = 70015;
+static const int PROTOCOL_VERSION = 70012;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70003;
+static const int MIN_PEER_PROTO_VERSION = 70005;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -42,7 +42,7 @@ static const int SENDHEADERS_VERSION = 70012;
 static const int FEEFILTER_VERSION = 70013;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 70014;
+static const int SHORT_IDS_BLOCKS_VERSION = 70012;
 
 //! not banning for invalid compact blocks starts with this version
 static const int INVALID_CB_NO_BAN_VERSION = 70012;
